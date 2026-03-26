3c77c838e968c74b2eb579c5f4f776baa0da569d
Serosh <janmejayapanda400@gmail.com>
2026-03-26 21:20:35 +0530

    making screens look good

--- include/DisplayEngine.h
+++ include/DisplayEngine.h
@@ -2,6 +2,7 @@
 #define DISPLAY_ENGINE_H
 
 #include "ProcessInfo.h"
+#include "SystemUtils.h"
 #include <vector>
 #include <map>
 #include <string>
@@ -12,15 +13,17 @@ namespace DisplayEngine {
                         double system_cpu_usage, double system_mem_usage, 
                         double system_uptime, int num_cores, 
                         const std::vector<Filter>& filters, bool logging_enabled, 
-                        const std::string& sort_criterion, const std::string& status_msg);
+                        const std::string& sort_criterion, const std::string& status_msg,
+                        const SystemUtils::CPULoadBreakdown& cpu_breakdown,
+                        const SystemUtils::MemBreakdown& mem_breakdown);
     
     void displayTree(WINDOW* win, pid_t pid, int depth, int &line, int max_lines, 
-                        int scroll_offset, int selected_row, 
+                        int scroll_offset, int h_scroll_offset, int selected_row, 
                         const std::map<pid_t, ProcessInfo>& process_map, 
                         const std::map<pid_t, std::vector<pid_t>>& process_tree);
     
-    void displayProcesses(WINDOW* win, int max_lines, int scroll_offset, 
+    void displayProcesses(WINDOW* win, int max_lines, int scroll_offset, int h_scroll_offset, 
                             int selected_row, const std::vector<ProcessInfo>& processes);
 }
 
-#endif // DISPLAY_ENGINE_H
+#endif
